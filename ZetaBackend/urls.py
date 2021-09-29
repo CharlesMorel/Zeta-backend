6ee3345996b53a07c25e1884e1f4c5c7f0f5d840
charlesmorelpro <83226882+charlesmorelpro@users.noreply.github.com>
2021-09-29 15:35:22 +0200

    add authentication to microservice

--- ZetaBackend/urls.py
+++ ZetaBackend/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
-from api.views import process_view
+from api.views import process_view, auth_view
 
 router = routers.DefaultRouter()
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('auth/', auth_view.AuthenticationView.as_view()),
     path('process/', process_view.ProcessList.as_view()),
     path('process/<int:pk>', process_view.ProcessDetail.as_view()),
 ]
