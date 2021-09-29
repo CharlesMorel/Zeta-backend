b0f8245d16523a40216e580b22d952a11ac53535
henri <henri.briaux@viacesi.fr>
2021-09-29 15:19:15 +0200

    Auth without useless import

--- ZetaBackend/settings.py
+++ ZetaBackend/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import ldap
-from django_auth_ldap.config import LDAPSearch
-import ldapdb
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
