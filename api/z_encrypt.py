aedc51ef8d53a57343f0d65957ea0630f9184edf
henri <henri.briaux@viacesi.fr>
2021-10-01 08:51:05 +0200

    commented algo

--- api/z_encrypt.py
+++ api/z_encrypt.py
@@ -2,9 +2,14 @@ import numpy as np
 def z_decrypt(e_data):
     KEY=3
     e_data=[w for w in e_data]
+
+    # Change each character to unicode value with KEY
     e_data=[ord(i)-KEY for i in e_data]
+    # Reverse tab
     e_data=e_data[::-1]
+    # Change unicode value to character with KEY
     e_data=[chr(i-KEY) for i in e_data]
+    # Roll tab with KEY
     e_data=np.roll(e_data, -KEY)
 
     return e_data
@@ -12,9 +17,14 @@ def z_decrypt(e_data):
 def z_encrypt(data):
     KEY=3
     e_data=[w for w in str(data)]
+
+    # Roll tab with KEY
     e_data=np.roll(e_data, KEY)
-    e_data=[ord(i)+KEY for i in e_data] 
+    # Change each character to unicode value with KEY
+    e_data=[ord(i)+KEY for i in e_data]
+    # Reverse tab 
     e_data=e_data[::-1]
+    # Change unicode value to character with KEY
     e_data=[chr(i+KEY) for i in e_data]
 
     return e_data
